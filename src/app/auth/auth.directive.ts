07a33baddbed1bfb006b07c140eb62cd1341681f
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-27 11:35:57 +0530

    2

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, effect, inject, input } from '@angular/core';
+import { Permission } from './auth.model';
+import { AuthService } from './auth.service';
 
 @Directive({
   selector: '[appAuth]'
 })
 export class AuthDirective {
+userType=input.required<Permission>();
+private authService = inject(AuthService);
 
-  constructor() { }
+constructor() {
+  effect(() =>{
+    if(this.authService.activePermission() === this.userType)
+    {
+      console.log('Show Element');
+   }else{
+    console.log('DO NOT SHOW ELEMENT');
+    }
+  })
+ }
 
 }
