a3e65e792ba6233d545a514a53c5e70564d72f74
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-27 16:50:59 +0530

    k

--- src/app/display.pipe.ts
+++ src/app/display.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from "@angular/core";
+import { DatePipe } from '@angular/common';
 @Pipe({
     name:'display',
     standalone:true
 })
 export class DisplayFont implements PipeTransform{
+    constructor(dt:DatePipe){}
     transform(dis:{ name : string, date : Date}){
-        return `${dis.name} +  logged in at +(${dis.date.toDateString()})`;
+        
+        return `${dis.name} + "logged in at" + this.dt.transform(${dis.date.toDateString()}` ;
     }
+
 }
